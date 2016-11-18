7650d1d336e0f1486bb5990607e5dcbaa5950eb2
Edern76 <gawein.le@live.fr>
2016-11-18 12:19:13 +0100

    Added setup script for Anaconda
    Excluded some of the unnecessary Anaconda modules.

---
+++ setupconda.py
@@ -0,0 +1,12 @@
+from cx_Freeze import setup, Executable #, version
+ 
+ 
+build_exe_options = {"packages": ["cffi"], "excludes": ["jupyter", "scipy", "numpy","nose",]}
+bdist_msi_options = {'add_to_path' : True} 
+setup(
+    name ='ColorTest',
+    version ='1.0',
+    description ='Shows the result of various RGB combinations.',
+    options = {"build_exe": build_exe_options, "bdist_msi": bdist_msi_options},
+    executables = [Executable("main.py")],
+    )
